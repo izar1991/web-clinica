0ab3077baf597296d53783e02c4db36cf324f364
Izar de Villasante LLaquet <izarvillasante@gmail.com>
2026-08-15 14:33:34 +0000

    preparar para multilingue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ export default defineConfig({
   site: 'https://izar1991.github.io',
   base: '/web-clinica',
   trailingSlash: 'ignore',
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'ca'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
